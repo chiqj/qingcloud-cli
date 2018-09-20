57e528740497eb6aef0617c70262b07e43300f3d
Invoker <chiqingjun@gmail.com>
2018-09-20 15:25:23 +0800

    flake8 check

--- src/iaas.py
+++ src/iaas.py
@@ -10,7 +10,7 @@ def validate_config_file(ctx, param, value):
     path = os.path.expanduser(value)
     # 检查文件存在
     if not os.path.isfile(path):
-        raise click.BadParameter(f"Config file \"{path}\" not exists.")
+        raise click.BadParameter(f"Config file \"{path}\" not exists.")     # noqa: ignore=E999
     # 读取
     try:
         with open(path, "r") as f:
@@ -19,7 +19,7 @@ def validate_config_file(ctx, param, value):
             "qy_access_key_id": result["qy_access_key_id"],
             "qy_secret_access_key": result["qy_secret_access_key"],
         }
-    except:
+    except:         # noqa: ignore=E722
         raise click.BadParameter(f"Config file \"{path}\" format error.")
 
 
